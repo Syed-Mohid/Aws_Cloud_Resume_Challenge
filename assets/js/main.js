25e29d582f006e6c7cd332b663d9ae647d4bb876
Mohid <syedmohid314@gmail.com>
2023-11-13 21:45:08 -0500

    Uploading files using git

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function($) {
+
+
+	var mastheadheight = $('.ds-header').outerHeight();
+	//console.log(mastheadheight);
+	$(".ds-banner,.ds-main-section").css("margin-top" , mastheadheight);
+
+	$(window).scroll(function(){
+	    if ($(window).scrollTop() >= 10) {
+	        $('.ds-header').addClass('ds-fixed-header');
+	    }
+	    else {
+	        $('.ds-header').removeClass('ds-fixed-header');
+	    }
+	}).scroll();
+
+
+});
+
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch(
+        "https://25jzpqaq6robvcw42ctua562x40pyxre.lambda-url.ca-central-1.on.aws/"
+    );
+    let data = await response.json();
+    counter.innerHTML = `👀 Views: ${data}`;
+}
+updateCounter();
